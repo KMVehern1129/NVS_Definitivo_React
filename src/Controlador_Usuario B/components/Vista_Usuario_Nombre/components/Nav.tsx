5ee092fc559a649c3867d00a488fab805c2a9e4c
Karen-alvarez04 <alvarezricok@gmail.com>
2025-04-10 16:59:43 -0500

    Creacion de Controlador_Usuario B / Separacion por componentes / Carga de base de datos (14)

---
+++ src/Controlador_Usuario B/components/Vista_Usuario_Nombre/components/Nav.tsx
@@ -0,0 +1,19 @@
+const Nav = () => (
+    <nav id="Menu_Principal">
+      <div className="Imagen_Logotipo_NVS">
+        <img src="../Sin título-positiu.svg" alt="Logotipo de NVS" />
+      </div>
+  
+      <ul>
+        <li><a href="" className="Btn_Nav_Uno">Inicio</a></li>
+        <li><a href="" className="Btn_Nav_Dos">Videojuegos</a></li>
+        <li><a href="" className="Btn_Nav_Tres">Consolas</a></li>
+        <li><a href="" className="Btn_Nav_Cuatro">Nintendo</a></li>
+        <li><a href="" className="Btn_Nav_Cinco">PlayStation</a></li>
+        <li><a href="" className="Btn_Nav_Séis">Xbox</a></li>
+      </ul>
+    </nav>
+  );
+  
+  export default Nav;
+  
