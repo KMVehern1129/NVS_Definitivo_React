367afd74479a7111461f663b5b216ce731f9be50
Karen-alvarez04 <alvarezricok@gmail.com>
2025-04-23 17:39:59 -0500

    Agregar imagenes (Organizar estilos)

--- src/Pages/Vista_Controlador_Usuario.tsx
+++ src/Pages/Vista_Controlador_Usuario.tsx
@@ -2,6 +2,14 @@ import '../css/Css-Usuarios/styles.css';
 
 import { Outlet, Link } from 'react-router-dom';
 
+// Imagenes
+import Usuario from '@assets/imgs/Cont_Usu_B/Usu.png';
+import Basura from '@assets/imgs/Cont_Usu_B/Basura.png';
+import Arroba from '@assets/imgs/Cont_Usu_B/Arroba.png';
+import Candado from '@assets/imgs/Cont_Usu_B/candado.png';
+import Usuario_Abajo from '@assets/imgs/Cont_Usu_B/Usuario_Nav_Abajo.png';
+import Ubicacion from '@assets/imgs/Cont_Usu_B/Ubicacion.png';
+
 const VistaUsuarioCorreo = () => {
   return (
     <div className="Container_Principal">
@@ -9,7 +17,7 @@ const VistaUsuarioCorreo = () => {
           <header className="header-main">
             <figure>
               <div className="Imagen_Usuario">
-                <img src="../Usu.png" alt="Logo Usuario" />
+                <img src={Usuario} alt="Logo Usuario" />
               </div>
               <section>
                 <div className="Datos_Conjuntos_Img_Usuario">
@@ -38,7 +46,7 @@ const VistaUsuarioCorreo = () => {
                     <p>ADMIN.</p>
                   </article>
                   <p>Gracias por tu comentario, lo tendremos en cuenta.</p>
-                  <button><img src="../Basura.png" alt="Icono Basura" /></button>
+                  <button><img src={Basura} alt="Icono Basura" /></button>
                 </section>
                 <section id="Two">
                   <article className="Texto_Pop_up">
@@ -46,7 +54,7 @@ const VistaUsuarioCorreo = () => {
                     <p>ANUNCIOS</p>
                   </article>
                   <p>Semana de descuentos en todos los juegos de playstation 5.</p>
-                  <button><img src="../Basura.png" alt="Icono Basura" /></button>
+                  <button><img src={Basura} alt="Icono Basura" /></button>
                 </section>
                 <section id="Three">
                   <article className="Texto_Pop_up">
@@ -54,7 +62,7 @@ const VistaUsuarioCorreo = () => {
                     <p>ANUNCIOS</p>
                   </article>
                   <p>Aprovecha el black friday y llevate 3 videojuegos en físico por el precio de 2 en nuestras tiendas.</p>
-                  <button><img src="../Basura.png" alt="Icono Basura" /></button>
+                  <button><img src={Basura} alt="Icono Basura" /></button>
                 </section>
               </div>
             </div>
@@ -64,10 +72,10 @@ const VistaUsuarioCorreo = () => {
             <section id="Uno">
               <nav className="Menu_Usuario">
                 <div className="Imagenes_Menu_Usuario">
-                  <img src="../Arroba.png" alt="" />
-                  <img src="../candado.png" alt="" />
-                  <img src="../Usuario_Nav_Abajo.png" alt="" />
-                  <img src="../Ubicacion.png" alt="" />
+                  <img src={Arroba} alt="" />
+                  <img src={Candado} alt="" />
+                  <img src={Usuario_Abajo} alt="" />
+                  <img src={Ubicacion} alt="" />
                 </div>
                 <ul>
                   <li><Link to="/Usuario/Correo">Cambiar e-mail</Link></li>
